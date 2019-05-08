78f1a23324da0fa5abdb93e4ed5ff84e12937437
Jason Irish <jsirish@users.noreply.github.com>
2019-05-08 09:57:55 -0500

    initial README (#4)

--- src/Model/Brochure.php
+++ src/Model/Brochure.php
@@ -2,10 +2,14 @@
 
 namespace Dynamic\Products\Model;
 
+use Dynamic\Products\Extension\ProductFileDataExtension;
+
 class Brochure extends ProductFile
 {
     /**
      * @var string
      */
     private static $table_name = 'Brochure';
+    
+    private static $extensions = [ProductFileDataExtension::class];
 }
