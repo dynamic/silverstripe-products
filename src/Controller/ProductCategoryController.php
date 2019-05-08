e0a3553db618213848e948ef65451d89b41acd3d
Jason Irish <jirish@dynamicagency.com>
2019-05-08 09:45:31 -0500

    ProductCategory - created PagingatedList of Products
    ```
    <% loop $PaginatedList %>
    ```

--- src/Controller/ProductCategoryController.php
+++ src/Controller/ProductCategoryController.php
@@ -2,7 +2,29 @@
 
 namespace Dynamic\Products\Page;
 
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\ORM\PaginatedList;
+
 class ProductCategoryController extends \PageController
 {
+    /**
+     * @param HTTPRequest|null $request
+     * @return PaginatedList
+     */
+    public function paginatedList(HTTPRequest $request = null)
+    {
+        if (!$request instanceof HTTPRequest) {
+            $request = $this->getRequest();
+        }
+        $recipes = $this->data()->getProductList();
+        $start = ($request->getVar('start')) ? (int)$request->getVar('start') : 0;
+        $records = PaginatedList::create($recipes, $request);
+        $records->setPageStart($start);
+        $records->setPageLength($this->data()->ProductsPerPage);
 
+        // allow $records to be updated via extension
+        $this->extend('updatePaginatedList', $records);
+
+        return $records;
+    }
 }
--- src/Page/ProductCategory.php
+++ src/Page/ProductCategory.php
@@ -2,7 +2,69 @@
 
 namespace Dynamic\Products\Page;
 
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\NumericField;
+
 class ProductCategory extends \Page
 {
+    /**
+     * @var array
+     */
+    private static $db = [
+        'ProductsPerPage' => 'Int',
+    ];
 
+    /**
+     * @var array
+     */
+    private static $defaults = [
+        'ProductsPerPage' => 12,
+    ];
+
+    /**
+     * @var string
+     */
+    private static $table_name = 'ProductCategory';
+
+    /**
+     * @var array
+     */
+    private static $allowed_children = [
+        ProductCategory::class,
+        Product::class,
+    ];
+
+    /**
+     * @return \SilverStripe\Forms\FieldList
+     */
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function (FieldList $fields) {
+            $fields->addFieldToTab(
+                'Root.Main',
+                NumericField::create('ProductsPerPage')
+                    ->setTitle(_t(__CLASS__ . '.ProductsPerPage', 'Products Per Page')),
+                'Content'
+            );
+        });
+
+        return parent::getCMSFields();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProductList()
+    {
+        $categories = ProductCategory::get()->filter('ParentID', $this->data()->ID)->column('ID');
+        $categories[] = $this->data()->ID;
+        $products = Product::get()
+            ->filterAny([
+                'ParentID' => $categories,
+            ]);
+
+        $this->extend('updateProductList', $products);
+
+        return $products;
+    }
 }
