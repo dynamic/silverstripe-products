8d997bfefb744f79491ce53f22c89388ff60db6d
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2019-12-17 14:56:25 -0600

    UPDATE tests

--- tests/Page/ProductCategoryTest.php
+++ tests/Page/ProductCategoryTest.php
@@ -5,6 +5,7 @@ namespace Dynamic\Products\Test\Page;
 use Dynamic\Products\Page\ProductCategory;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Security\Member;
 
 class ProductCategoryTest extends SapphireTest
 {
@@ -21,5 +22,23 @@ class ProductCategoryTest extends SapphireTest
         $object = $this->objFromFixture(ProductCategory::class, 'default');
         $fields = $object->getCMSFields();
         $this->assertInstanceOf(FieldList::class, $fields);
+    }
+
+    public function testGetProductList()
+    {
+        $this->logInAs($this->objFromFixture(Member::class, 'author'));
+
+        /** @var ProductCategory $category */
+        $category = $this->objFromFixture(ProductCategory::class, 'restricted');
+        $this->assertEquals(2, $category->getProductList()->count());
+
+        $this->logOut();
+        $this->logInAs($this->objFromFixture(Member::class, 'default'));
+        /** @var ProductCategory $category */
+        $category = $this->objFromFixture(ProductCategory::class, 'restricted');
+
+        $this->assertEquals(1, $category->getProductList()->count());
+
+        $this->logOut();
     }
 }
